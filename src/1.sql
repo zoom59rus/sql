a5f233032f85d8cdea80ed4bb4cbf67d2e148971
zoom59rus <anton_1@bk.ru>
2021-01-18 20:35:08 +0500

    1.sql

---
+++ src/1.sql
@@ -0,0 +1,29 @@
+-- CREATE TABLE person
+-- (
+--     id        int NOT NULL,
+--     firstName varchar(255),
+--     lastName  varchar(255),
+--     PRIMARY KEY (id)
+-- );
+--
+-- CREATE TABLE address
+-- (
+--     id       int NOT NULL,
+--     personId int,
+--     city     varchar(255),
+--     state    varchar(255),
+--     PRIMARY KEY (id)
+-- );
+--
+-- TRUNCATE TABLE person;
+--
+-- INSERT INTO person (id, firstName, lastName)
+-- VALUES (1, 'Wang', 'Allen');
+--
+-- TRUNCATE TABLE address;
+--
+-- INSERT INTO address (id, personId, city, state)
+-- VALUES (1, 2, 'New York City', 'New York');
+
+SELECT firstName, lastName, city, state
+FROM person INNER JOIN address on person.id = address.personid;
